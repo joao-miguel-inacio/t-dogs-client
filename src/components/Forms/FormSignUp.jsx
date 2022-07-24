97f8cf1a7c013c574d0049e36a73512d37b5d2da
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-07-24 09:15:33 +0100

    sign up form fully functional, buyer&&owner

--- src/components/Forms/FormSignUp.jsx
+++ src/components/Forms/FormSignUp.jsx
@@ -4,7 +4,10 @@ import service from "../../services/apiHandler";
 
 const FormSignUp = () => {
   
+  const [userType, setUserType] = useState("isBuyer");
+
   const [user, setUser] = useState({
+    userType: userType,
     name: "",
     email: "",
     password: "",
@@ -14,268 +17,80 @@ const FormSignUp = () => {
     hasPets: false,
   });
 
-  const [userType, setUserType] = useState ("buyer");
-  
   const displayBuyerSignUpForm = () => {
     return (
       <>
-        <button onClick={() => setUserType("owner")}>
-          I am looking to adopt a dog
-        </button>
-        {error && <h3 className="error">{error.message}</h3>}
-        <form onSubmit={handleSubmit}>
-          <h2>Signup</h2>
-          
-          <label htmlFor="name">Name</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={user.name}
-            type="text"
-            id="name"
-            name="name"
-          />
-  
-          <label htmlFor="email">Email</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={user.email}
-            type="email"
-            id="email"
-            name="email"
-          />
-  
-          <label htmlFor="password">Password</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={user.password}
-            type="password"
-            id="password"
-            name="password"
-          />
-  
-          <label htmlFor="address">Address</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={user.address}
-            type="text"
-            id="address"
-            name="address"
-          />
-  
-          <label htmlFor="hasChildren">Children?</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={true}
-            type="radio"
-            id="hasChildren"
-            name="hasChildren"
-          />
-          <label for="hasChildren">Yes</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={false}
-            type="radio"
-            id="!hasChildren"
-            name="hasChildren"
-            checked
-          />
-          <label for="!hasChildren">No</label>
-          
-  
-          <label htmlFor="hasExperience">Experience?</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={true}
-            type="radio"
-            id="hasExperience"
-            name="hasExperience"
-          />
-          <label for="hasExperience">Yes</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={false}
-            type="radio"
-            id="!hasExperience"
-            name="hasExperience"
-            checked
-          />
-          <label for="!hasExperience">No</label>
-  
-          <label htmlFor="hasPets">Pets?</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={true}
-            type="radio"
-            id="hasPets"
-            name="hasPets"
-          />
-          <label for="hasPets">Yes</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={false}
-            type="radio"
-            id="!hasPets"
-            name="hasPets"
-            checked
-          />
-          <label for="!hasPets">No</label>
-  
-          <button>Submit</button>
-        </form>
-      </>
-    );
-  }
+        <label htmlFor="hasChildren">Children?</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={true}
+          type="radio"
+          id="hasChildren"
+          name="hasChildren"
+        />
+        <label htmlFor="hasChildren">Yes</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={false}
+          type="radio"
+          id="!hasChildren"
+          name="hasChildren"
+          checked
+        />
+        <label htmlFor="!hasChildren">No</label>
 
-  const displayOwnerSignUpForm = () => {
-    setUserType ("buyer");
-    return (
-      <>
-      <button onClick={() => setUserType("buyer")}>
-        I am looking to rehome a dog
-      </button>
-        {error && <h3 className="error">{error.message}</h3>}
-        <form onSubmit={handleSubmit}>
-          <h2>Signup</h2>
-          
-          <label htmlFor="name">Name</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={user.name}
-            type="text"
-            id="name"
-            name="name"
-          />
-  
-          <label htmlFor="email">Email</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={user.email}
-            type="email"
-            id="email"
-            name="email"
-          />
-  
-          <label htmlFor="password">Password</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={user.password}
-            type="password"
-            id="password"
-            name="password"
-          />
-  
-          <label htmlFor="address">Address</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={user.address}
-            type="text"
-            id="address"
-            name="address"
-          />
-  
-          <label htmlFor="hasChildren">Children?</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={true}
-            type="radio"
-            id="hasChildren"
-            name="hasChildren"
-          />
-          <label for="hasChildren">Yes</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={false}
-            type="radio"
-            id="!hasChildren"
-            name="hasChildren"
-            checked
-          />
-          <label for="!hasChildren">No</label>
-          
-  
-          <label htmlFor="hasExperience">Experience?</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={true}
-            type="radio"
-            id="hasExperience"
-            name="hasExperience"
-          />
-          <label for="hasExperience">Yes</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={false}
-            type="radio"
-            id="!hasExperience"
-            name="hasExperience"
-            checked
-          />
-          <label for="!hasExperience">No</label>
-  
-          <label htmlFor="hasPets">Pets?</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={true}
-            type="radio"
-            id="hasPets"
-            name="hasPets"
-          />
-          <label for="hasPets">Yes</label>
-          <input
-            onChange={(e) =>
-              setUser({ ...user, [e.target.name]: e.target.value })
-            }
-            value={false}
-            type="radio"
-            id="!hasPets"
-            name="hasPets"
-            checked
-          />
-          <label for="!hasPets">No</label>
-  
-          <button>Submit</button>
-        </form>
+        <label htmlFor="hasExperience">Experience?</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={true}
+          type="radio"
+          id="hasExperience"
+          name="hasExperience"
+        />
+        <label htmlFor="hasExperience">Yes</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={false}
+          type="radio"
+          id="!hasExperience"
+          name="hasExperience"
+          checked
+        />
+        <label htmlFor="!hasExperience">No</label>
+
+        <label htmlFor="hasPets">Pets?</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={true}
+          type="radio"
+          id="hasPets"
+          name="hasPets"
+        />
+        <label htmlFor="hasPets">Yes</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={false}
+          type="radio"
+          id="!hasPets"
+          name="hasPets"
+          checked
+        />
+        <label htmlFor="!hasPets">No</label>
       </>
     );
-  }
+  };
 
   const [error, setError] = useState(null);
   const navigate = useNavigate();
@@ -293,11 +108,68 @@ const FormSignUp = () => {
 
   return (
     <>
-      {displayBuyerSignUpForm()}
+      {userType === "isBuyer" ? (
+        <button onClick={() => setUserType("isOwner")}>
+          I am looking to rehome a dog
+        </button>
+      ) : (
+        <button onClick={() => setUserType("isBuyer")}>
+          I am looking to adopt a dog
+        </button>
+      )}
+
+      {error && <h3 className="error">{error.message}</h3>}
+      <form onSubmit={handleSubmit}>
+        <h2>Signup</h2>
+
+        <label htmlFor="name">Name</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={user.name}
+          type="text"
+          id="name"
+          name="name"
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={user.email}
+          type="email"
+          id="email"
+          name="email"
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={user.password}
+          type="password"
+          id="password"
+          name="password"
+        />
+
+        <label htmlFor="address">Address</label>
+        <input
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          value={user.address}
+          type="text"
+          id="address"
+          name="address"
+        />
+        {userType === "isBuyer" ? displayBuyerSignUpForm() : ""}
+        <button>Submit</button>
+      </form>
     </>
-    
-  )
-  
+  );
 };
 
 export default FormSignUp;
